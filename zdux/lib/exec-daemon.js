d684635523804af793154b97706e7ef80f730912
Tenzin W. Lhakhang <tenzin.lhakhang@gmail.com>
2017-03-19 10:41:12 -0400

    Update exec-daemon.js
    replace variable name zfs with proc, code reuse from another stub project.  also removed semi-colons, not sure if i 100% like this.

--- zdux/lib/exec-daemon.js
+++ zdux/lib/exec-daemon.js
@@ -1,55 +1,58 @@
-'use strict';
+'use strict'
 let cp = require('child_process'),
-    spawn = cp.spawn;
-let EventEmitter = require('events').EventEmitter;
+    spawn = cp.spawn
+let EventEmitter = require('events').EventEmitter
 
 class ExecDaemon extends EventEmitter {
     constructor(options) {
-        super();
-        this.cmd = options.cmd;
-        this.args = options.args;
+        super()
+        this.cmd = options.cmd
+        this.args = options.args
+        this.interval = options.interval ? options.interval * 1000 : null
 
-        this.interval = options.interval ? options.interval * 1000 : null;
-
-        this.sleeping = true; // avoid starting another timer
-        this.halted = false; // control for non-zero exit code
+        this.sleeping = true // avoid starting another timer
+        this.halted = false // control for non-zero exit code
 
         // start execution of the command
-        this.timer = setInterval(execute, this.interval, this);
+        this.timer = setInterval(execute, this.interval, this)
     }
 }
 
 // exec method
 let execute = function(daemon) {
     if (daemon.sleeping && !daemon.halted) {
-        daemon.sleeping = false;
-        console.log('Executing %s', daemon.cmd);
+        daemon.sleeping = false
+        console.log('Executing %s', daemon.cmd)
 
-        let zfs = spawn(daemon.cmd, daemon.args);
-        let output = '';
-        let error = '';
+        let proc = spawn(daemon.cmd, daemon.args)
+        let output = ''
+        let error = ''
 
-        zfs.stdout.on('data', function(data) {
-            output = output.concat(data);
-        });
+        proc.stdout.on('data', function(data) {
+            output = output.concat(data)
+        })
 
-        zfs.stderr.on('data', function(data) {
-            error = error.concat(data);
-        });
+        proc.stderr.on('data', function(data) {
+            error = error.concat(data)
+        })
 
-        zfs.on('close', function(code) {
+        proc.on('close', function(code) {
             if (code == 0) {
-                daemon.halted = false;
-                daemon.sleeping = true;
-                daemon.emit('done', output.trim());
+                daemon.halted = false
+                daemon.sleeping = true
+                daemon.emit('done', output.trim())
             } else {
                 // error has occured
-                daemon.halted = true; // broken state
-                daemon.sleeping = false;
+                daemon.halted = true// broken state
+                daemon.sleeping = false
                 daemon.emit('error', error.trim())
             }
-        });
+        })
+        
+        proc.on('error', function(error) {
+            daemon.emit('error', error.trim())
+        }
     }
 }
 
-module.exports = ExecDaemon;
+module.exports = ExecDaemon
